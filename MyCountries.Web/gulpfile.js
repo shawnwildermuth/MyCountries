e4d754a2bd6dc150d8c705df675c078291c21911
Shawn Wildermuth <shawn@wildermuth.com>
2015-01-16 18:25:50 +0200

    Converted to gulp and ngannotate for angular code

---
+++ MyCountries.Web/gulpfile.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+ 
+var gulp = require('gulp');
+var diff = require('gulp-diff');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var ngAnnotate = require('gulp-ng-annotate');
+var bower = require("gulp-bower");
+ 
+gulp.task('default', function () {
+
+    return gulp.src('wwwroot/js/*.js')
+        .pipe(ngAnnotate())
+        //.pipe(diff())
+        //.pipe(diff.reporter())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('wwwroot/lib/_app'));
+
+});
+
+gulp.task("bower", function () {
+  return bower()
+    .pipe(gulp.dest("wwwroot/lib/"));
+});
